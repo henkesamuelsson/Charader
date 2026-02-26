a4a86f6f75e76b0da830d665452153e0f4032ece
henkesamuelsson <henke.samuelsson@gmail.com>
2026-02-26 15:33:27 +0100

    Uppdaterad kortlistor för barnkategorierna.

--- src/cards/jobs.js
+++ src/cards/jobs.js
@@ -1,6 +1,6 @@
 export const cards = [
   "Brandman", "Astronaut", "Kock", "Kirurg", "Pilot",
-  "Clown", "Magiker", "Dykare", "Detektiv", "Cowboy",
+  "Clown", "Magiker", "Dykare", "Detektiv", "Polis",
   "Frisör", "Tandläkare", "Snickare", "Lärare", "Bonde",
-  "Rockstjärna", "Balettdansör", "Brevbärare", "Fyrvaktare", "Skoputsare",
+  "Rockstjärna", "Balettdansör", "Brevbärare", "Fyrvaktare", "Skomakare",
 ]
--- src/cards/kidsHome.js
+++ src/cards/kidsHome.js
@@ -1,5 +1,9 @@
 export const cards = [
-  "Soffa", "Dammsugare", "Tandborste", "Kudde", "Filt",
-  "Spegel", "Lampa", "Kylskåp", "Tvättmaskin", "Toalett",
-  "Badkar", "Telefon", "TV-Apparat", "Mikrovågsugn", "Trappor",
-]
+  "Badkar", "Bok", "Bokhylla", "Boll", "Bord", "Brödrost", "Dammsugare", "Diskborste", 
+  "Dörr", "Filt", "Fjärrkontroll", "Fönster", "Gaffel", "Gardiner", "Handduk", "Hatt", 
+  "Hink", "Hårborste", "Jacka", "Kastrull", "Klocka", "Kudde", "Kylskåp", "Lampa", 
+  "Leksaksbil", "Ljus", "Matta", "Mikrovågsugn", "Mugg", "Nalle", "Papper", "Paraply", 
+  "Penna", "Ryggsäck", "Schampo", "Sked", "Skrivbord", "Soffa", "Sopborste", "Soptunna", 
+  "Spelkontroll", "Spegel", "Stekpanna", "Stol", "Säng", "Tallrik", "Tandborste", 
+  "Telefon", "Toalett", "TV Apparat", "Tvål", "Tvättmaskin", "Vattenflaska", "Väska"
+];
